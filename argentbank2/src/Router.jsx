f5a2731da419ab17baf4e644e9b0899652e3518a
IsmaSEO <ismail.jelassi.dev@gmail.com>
2025-02-20 21:46:08 +0100

    Ajout de la navigation et des routes principales avec React Router

---
+++ argentbank2/src/Router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+import UserProfil from "./pages/UserProfil/UserProfil";
+
+const AppRouter = () => {
+  const routes = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/signin", element: <SignIn /> },
+    { path: "/signup", element: <SignUp /> },
+    { path: "/profile", element: <UserProfil /> },
+    { path: "/not-found", element: <NotFound /> },
+    { path: "*", element: <NotFound /> },
+  ]);
+
+  return <RouterProvider router={routes} />;
+};
+
+export default AppRouter;
