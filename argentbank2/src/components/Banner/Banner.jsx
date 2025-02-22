dd67f6f050aaccf3ede44a2f060a689f42376fce
IsmaSEO <ismail.jelassi.dev@gmail.com>
2025-02-22 08:30:03 +0100

    Ajout du composant Banner

---
+++ argentbank2/src/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import "./Banner.css";
+
+const Banner = () => (
+  <section className="banner">
+    <div className="banner-content">
+      <h1 className="banner-title">
+        No fees. <br />
+        No minimum deposit. <br />
+        High interest rates.
+      </h1>
+      <p className="banner-text">
+        Open a savings account with Argent Bank today!
+      </p>
+    </div>
+  </section>
+);
+
+export default Banner;
